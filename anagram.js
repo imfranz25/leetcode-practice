5e3d81f7e220dc0dac07eb7e1d804a4589f51da4
“Francis” <francis.ong25@gmail.com>
2025-02-20 20:05:01 +0800

    feat: remove cmt

--- anagram.js
+++ anagram.js
@@ -1,15 +1,11 @@
 function anagram(p1, p2) {
-  // Remove any non-alphabetic characters and convert to lowercase
   const normalize = (str) => str.replace(/[^\w]/g, "").toLowerCase();
-
-  // Sort the characters of the string
   const sorted = (str) => normalize(str).split("").sort().join("");
 
-  // Compare the sorted strings
   return sorted(p1) === sorted(p2);
 }
 
-const p1 = "anagram ";
+const p1 = "anagram";
 const p2 = "nagaram";
 
 console.log("p1:", p1);
